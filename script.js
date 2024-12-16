68e4cc29cc41c58fca6728c36c3bef3493293028
ElMontenegro <elmontenegro46@gmail.com>
2024-12-16 11:42:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var toMareepPage = function () {
+  window.location.href = "./mareep_sticker.html";
+};
+
+var toHome = function () {
+  window.location.href = "./index.html";
+};
+
+var toCart = function () {
+  window.location.href = "./cart.html";
+};
+
+var toContact = function () {
+  window.location.href = "./contact.html";
+};
+
+var toChainsawPage = function () {
+  window.location.href = "./chainsawman.html";
+};
+var toRineaPage = function () {
+  window.location.href = "./rinea.html";
+};
+var toLinkPage = function () {
+  window.location.href = "./linktotk.html";
+};
